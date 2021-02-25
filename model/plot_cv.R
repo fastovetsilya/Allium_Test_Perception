e926c58ef09732b9e20810539735b8dfbb9cf850
Ilya <ilya.fastovets@gmail.com>
2021-02-24 18:50:00 -0800

    Improve R plot for cross-validation

--- model/plot_cv.R
+++ model/plot_cv.R
@@ -36,17 +36,19 @@ rownames(stats_data) <- val_groups
 # Make a ggplot
 png(paste0(cv_dir, 'CV_plot.png'))
 ggplot(stats_data, aes(x = iteration, y = val_means)) +
-  geom_errorbar(aes(ymin=val_CI_low, ymax=val_CI_high), width=100) +
-  geom_line() +
+  geom_errorbar(aes(ymin=val_CI_low, ymax=val_CI_high), width=1) +
+  geom_line(size = 1) +
   geom_point() +
-  geom_smooth() + 
+  geom_smooth(size = 0.5) + 
   geom_text(aes(label = round(val_means, 2)), col = 'red') +
   geom_text(aes(label = round(val_CI_low, 2)), vjust = 2) +
   geom_text(aes(label = round(val_CI_high, 2)), vjust = -1) +
   ggtitle('Cross-validation mAP (mean average precision)') +
   xlab('Iteration') +
   ylab('CV mAP@0.50') + 
-  xlim(c(0, max(val_groups))) + 
-  geom_hline(yintercept=max(val_means), linetype="dashed", color = "blue", size=0.5) + 
-  geom_hline(yintercept=summary(val_means)[2], linetype="dashed", color = "darkgrey", size=0.5)
+  geom_hline(yintercept=max(val_means), linetype="dashed", color = "blue", size=0.5) +
+  geom_hline(yintercept=summary(val_means)[2], linetype="dashed", color = "darkgrey", size=0.5) +
+  geom_vline(xintercept=val_result$n[which.max(val_means)], linetype="dashed", color = "blue", size=0.5) +
+  scale_x_continuous(breaks = val_result$n, minor_breaks = NULL, limits = c(0, max(val_groups))) + 
+  scale_y_continuous(breaks = seq(0, 1, 0.1), minor_breaks = NULL, limits = c(0, 1))
 dev.off()
